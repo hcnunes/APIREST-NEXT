55d27c9d11b0caf8ee1997f7bfd655dec43cdddc
Bruno Paes <89491247+paesbruno@users.noreply.github.com>
2021-10-22 10:30:37 -0300

    update2210

--- project/src/main/java/project/entity/Projeto.java
+++ project/src/main/java/project/entity/Projeto.java
@@ -1,5 +1,53 @@
 package project.entity;
 
-public class Projeto {
+import java.sql.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table (schema = "next_rest_api_schema", name = "projeto", uniqueConstraints = {@UniqueConstraint (name = "uk_nome_projeto", columnNames = { "nome_projeto" }) })
+
+public class Projeto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	
+	@Column(name = "id_projeto", nullable = false, updatable = false)
+    private Long id_projeto;
+	
+	@Column(name = "nome_projeto", nullable = false, length = 100)
+    private String nome_projeto;
+	
+	@Column(name = "gerente_projeto", nullable = false)
+    private Long gerente_projeto;
+	
+	@Column(name = "centro_custo_projeto", nullable = false, length = 100)
+    private String centro_custo_projeto;
+	
+	@Column(name = "data_inicio_projeto", nullable = false)
+    private Date data_inicio_projeto;
+	
+	@Column(name = "data_termino_projeto", nullable = false)
+    private Date data_termino_projeto;
+	
+	@Column(name = "status_projeto", nullable = false, length = 100)
+    private String status_projeto;
+	
+	@Column(name = "flag_projeto", nullable = false, length = 100)
+    private String flag_projeto;
+
+	public Long getId() {
+        return this.id_projeto;
+    }
+	
+	public void setName(String name) {
+        this.nome_projeto = name;
+    }
+	
 }
--- project/src/main/java/project/entity/Usuario.java
+++ project/src/main/java/project/entity/Usuario.java
@@ -1,5 +1,7 @@
 package project.entity;
 
+import java.sql.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,7 +11,7 @@ import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table (schema = "next_rest_api_schema", name = "usuario", uniqueConstraints = {@UniqueConstraint (name = "uk_primeiro_nome", columnNames = { "name" }) })
+@Table (schema = "next_rest_api_schema", name = "usuario", uniqueConstraints = {@UniqueConstraint (name = "uk_primeiro_nome", columnNames = { "primeiro_nome" }) })
 
 public class Usuario {
 
@@ -18,15 +20,31 @@ public class Usuario {
     @Column(name = "id", nullable = false, updatable = false)
     private Long id;
 
-    @Column(name = "name", nullable = false, length = 100)
-    private String name;
+    @Column(name = "primeiro_nome", nullable = false, length = 100)
+    private String primeiro_nome;
+    
+    @Column(name = "ultimo_nome", nullable = false, length = 100 )
+    private String ultimo_nome;
+    
+    @Column(name = "matricula", nullable = false, length = 4 )
+    private String matricula;
+    
+    @Column(name = "data_nascimento", nullable = false)
+    private Date data_nascimento;
+    
+    @Column(name = "ativo", nullable = false, length = 10)
+    private String ativo;
+    
+    @Column(name = "centro_custo", nullable = false, length = 50)
+    private String centro_custo;
+        
     
     public Long getId() {
         return this.id;
     }
     
     public String getName() {
-        return this.name;
+        return this.primeiro_nome;
     }
 	
 }
