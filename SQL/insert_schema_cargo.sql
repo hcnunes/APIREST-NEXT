6041d1e50eebb9c46460406a67dca591426c18af
Bruno Paes <89491247+paesbruno@users.noreply.github.com>
2021-10-25 19:12:24 -0300

    organizeSchemaSql

---
+++ SQL/insert_schema_cargo.sql
@@ -0,0 +1,3 @@
+INSERT INTO next_rest_api_schema.cargo(nome) VALUES ('Gerente');
+INSERT INTO next_rest_api_schema.cargo(nome) VALUES ('Analista Júnior');
+INSERT INTO next_rest_api_schema.cargo(nome) VALUES ('Analista Pleno');
